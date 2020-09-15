673981dfa84b6f89fe488f733bb2272bf264a3ce
naimasa <naimasa@gmail.com>
2020-09-15 13:14:51 +0900

    fix the command argument extraction issue

--- update-ytmusic-jwave-playlist.sh
+++ update-ytmusic-jwave-playlist.sh
@@ -5,16 +5,24 @@ source venv/bin/activate
 
 echo "Generating J-Wave songlist..."
 jwave_songlist=jwave-songlist-`date -v-1d +%Y%m%d`.json
-python3 jwave-songlist.py > $jwave_songlist
-echo "Done."
+if [ ! -e $jwave_songlist ];then
+    python3 jwave-songlist.py > $jwave_songlist
+    echo "Done."
+else
+    echo "Skipped."
+fi
 
 echo "Generating YouTube Music songlist..."
 ytmusic_songlist=ytmusic-songlist-`date -v-1d +%Y%m%d`.json
-python3 ytmusic-tracks.py headers_auth.json $jwave_songlist > $ytmusic_songlist
-echo "Done."
+if [ ! -e $ytmusic_songlist ];then
+    python3 ytmusic-tracks.py headers_auth.json $jwave_songlist > $ytmusic_songlist
+    echo "Done."
+else
+    echo "Skipped."
+fi
 
 echo "Updating YouTube Music Playlist..."
-playlist_name="'J-Wave Songlist'"
-playlist_description="'J-Wave Songlist `date -v-1d +%Y%m%d`'"
-python3 ytmusic-playlist.py headers_auth.json $ytmusic_songlist $playlist_name $playlist_description --remove_all_tracks_before_add
+playlist_name="J-Wave Songlist"
+playlist_description="J-Wave Songlist `date -v-1d +%Y%m%d`"
+python3 ytmusic-playlist.py headers_auth.json $ytmusic_songlist "$playlist_name" "$playlist_description" --remove_all_tracks_before_add
 echo "Done."
